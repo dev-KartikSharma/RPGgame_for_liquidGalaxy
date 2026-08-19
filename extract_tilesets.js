eb78becdda339fe78b96112285681c7db3a67855
Kartik Sharma <kartiksharma17012007@gmail.com>
2026-08-19 19:20:36 +0530

    clean: remove non-essential documentation and helper scripts, and update Liquid Galaxy layout to clockwise configuration

--- extract_tilesets.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const p = path.join('C:\\Users\\HP\\Desktop\\RPG Game\\rpg-game-lg\\public\\maps\\spawn.json');
-const map = JSON.parse(fs.readFileSync(p, 'utf8'));
-
-console.log("Tilesets in spawn.json:");
-map.tilesets.forEach(ts => {
-    console.log(`- tiledName: '${ts.name}', image: '${ts.image}'`);
-});
